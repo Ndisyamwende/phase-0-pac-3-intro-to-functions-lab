0f37b8d5de8d7520dc5be60864d2be2fe4445b5f
Ndisyamwende <sharonndisya2002@gmail.com>
2023-12-10 18:27:13 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,19 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+function whisper(string) {
+return string.toLowerCase();
+
+}
+
+function logShout(string) {
+    return string.toUpperCase();
+
+}
+var f = function() {
+    console.log("Hello world"):
+}
+
+function SayHiToGrandma(string) {
+    return string.toUpperCase();
+}
